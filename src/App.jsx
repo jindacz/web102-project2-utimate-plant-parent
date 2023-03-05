0dbcf383aecc1c1ad4b21e0d5bceae678b664c49
jindacz <jindaz.work@outlook.com>
2023-03-04 20:10:05 -0800

    diff level

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,55 @@ import './App.css';
 import samosaPic from "./assets/samosa.jpeg"
 import Card from "./components/Card";
 
+const cards = [
+  { 
+    question: 'What is the capital of France?', 
+    answer: 'Paris', 
+    image: 'http://placekitten.com/200/300',
+    category: 'Geography',
+    difficulty: 'Easy',
+  },
+  { 
+    question: 'What is the largest mammal in the world?', 
+    answer: 'Blue whale', 
+    image: 'http://placekitten.com/200/300',
+    category: 'Science',
+    difficulty: 'Medium',
+  },
+  { 
+    question: 'What is the smallest country in the world?', 
+    answer: 'Vatican City', 
+    image: 'http://placekitten.com/200/300',
+    category: 'Geography',
+    difficulty: 'Hard',
+  },
+  { 
+    question: 'What is the highest mountain in the world?', 
+    answer: 'Mount Everest',
+    image: 'http://placekitten.com/200/300',
+    category: 'Geography',
+    difficulty: 'Medium',
+  },
+  { 
+    question: 'What is the largest desert in the world?', 
+    answer: 'Sahara',
+    image: 'http://placekitten.com/200/300',
+    category: 'Geography',
+    difficulty: 'Easy',
+  },
+  { 
+    question: 'What is the largest desert in the world?', 
+    answer: 'Sahara',
+    image: 'http://placekitten.com/200/300',
+    category: 'Geography',
+    difficulty: 'Easy',
+  },
+];
 const App = () => {
-  const cards = [
-    { question: 'What is the capital of France?', answer: 'Paris' },
-    { question: 'What is the largest mammal in the world?', answer: 'Blue whale' },
-    { question: 'What is the smallest country in the world?', answer: 'Vatican City' },
-    { question: 'What is the highest mountain in the world?', answer: 'Mount Everest' },
-    { question: 'What is the largest desert in the world?', answer: 'Sahara' },
-    { question: 'What is the capital of France?', answer: 'Paris' },
-    { question: 'What is the largest mammal in the world?', answer: 'Blue whale' },
-    { question: 'What is the smallest country in the world?', answer: 'Vatican City' },
-    { question: 'What is the highest mountain in the world?', answer: 'Mount Everest' },
-    { question: 'What is the largest desert in the world?', answer: 'Sahara' },
-  ]
-  const count = cards.length;
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
   const [showAnswer, setShowAnswer] = useState(false);
 
   const currentCard = cards[currentCardIndex];
-
 
   const handleNextCard = () => {
     setShowAnswer(false);
@@ -34,11 +64,14 @@ const App = () => {
       <div className="header">
           <h1>The Ultimate Plant Parent!</h1>
           <h3>How good of a plant parent are you? Test all of your planty knowledge here!</h3>
-          <h3>Number of cards: {count}</h3>
+          <h3>Number of cards: {cards.length}</h3>
 
           <Card
             question={currentCard.question}
             answer={currentCard.answer}
+            image={currentCard.image}
+            category={currentCard.category}
+            difficulty={currentCard.difficulty}
             showAnswer={showAnswer}
             onCardClick={() => setShowAnswer(true)}/>
           <button onClick={handleNextCard}>Next</button>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,10 +2,30 @@ import React from 'react'
 import './Card.css'
 
 const Card = (props) => {
-	const {question, answer, showAnswer, onCardClick} = props;
-	return (
-	  <div className="card" onClick={onCardClick}>
-	   <div className='card' onClick={onCardClick}>
+	const {question, answer, showAnswer, onCardClick, image, category, difficulty} = props;
+
+
+
+	const getDifficultyColor = () => {
+		switch (difficulty) {
+			case 'Easy':
+				return 'green';
+			case 'Medium':
+				return 'orange';
+			case 'Hard':
+				return 'red'
+			default:
+				return 'black'
+		}
+	}
+
+	const cardStyle = {
+		backgroundColor: getDifficultyColor()
+	}
+	
+ 	return (
+	  <div className='container'>
+	   <div className='card' onClick={onCardClick} style={cardStyle}>
 			<div className='card-text'>{showAnswer ? answer : question}</div>
 	   </div>
 	  </div>
