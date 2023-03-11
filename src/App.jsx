cea9f03453be9da3e1ed3ce8eb734af9cd282380
jindacz <jindaz.work@outlook.com>
2023-03-10 22:12:00 -0800

    proj3

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,119 @@
 import { useState } from "react";
 
-import './App.css';
-import samosaPic from "./assets/samosa.jpeg"
+import "./App.css";
 import Card from "./components/Card";
 
 const cards = [
-  { 
-    question: 'What is the capital of France?', 
-    answer: 'Paris', 
-    image: 'http://placekitten.com/200/300',
-    category: 'Geography',
-    difficulty: 'Easy',
+  {
+    question: "What is the capital of France?",
+    answer: "Paris",
+    image: "http://placekitten.com/200/300",
+    category: "Geography",
+    difficulty: "Easy",
   },
-  { 
-    question: 'What is the largest mammal in the world?', 
-    answer: 'Blue whale', 
-    image: 'http://placekitten.com/200/300',
-    category: 'Science',
-    difficulty: 'Medium',
+  {
+    question: "What is the largest mammal in the world?",
+    answer: "Blue whale",
+    image: "http://placekitten.com/200/300",
+    category: "Science",
+    difficulty: "Medium",
   },
-  { 
-    question: 'What is the smallest country in the world?', 
-    answer: 'Vatican City', 
-    image: 'http://placekitten.com/200/300',
-    category: 'Geography',
-    difficulty: 'Hard',
+  {
+    question: "What is the smallest country in the world?",
+    answer: "Vatican City",
+    image: "http://placekitten.com/200/300",
+    category: "Geography",
+    difficulty: "Hard",
   },
-  { 
-    question: 'What is the highest mountain in the world?', 
-    answer: 'Mount Everest',
-    image: 'http://placekitten.com/200/300',
-    category: 'Geography',
-    difficulty: 'Medium',
+  {
+    question: "What is the highest mountain in the world?",
+    answer: "Mount Everest",
+    image: "http://placekitten.com/200/300",
+    category: "Geography",
+    difficulty: "Medium",
   },
-  { 
-    question: 'What is the largest desert in the world?', 
-    answer: 'Sahara',
-    image: 'http://placekitten.com/200/300',
-    category: 'Geography',
-    difficulty: 'Easy',
+  {
+    question: "What is the largest desert in the world?",
+    answer: "Sahara",
+    image: "http://placekitten.com/200/300",
+    category: "Geography",
+    difficulty: "Easy",
   },
-  { 
-    question: 'What is the largest desert in the world?', 
-    answer: 'Sahara',
-    image: 'http://placekitten.com/200/300',
-    category: 'Geography',
-    difficulty: 'Easy',
+  {
+    question: "What is the largest desert in the world?",
+    answer: "Sahara",
+    image: "http://placekitten.com/200/300",
+    category: "Geography",
+    difficulty: "Easy",
   },
 ];
+
 const App = () => {
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
   const [showAnswer, setShowAnswer] = useState(false);
+  const [guess, setGuess] = useState("");
+  const [feedback, setFeedback] = useState(null);
 
   const currentCard = cards[currentCardIndex];
 
   const handleNextCard = () => {
     setShowAnswer(false);
-    setCurrentCardIndex(Math.floor(Math.random()* cards.length));
-  }
+    setGuess("");
+    setFeedback(null);
+    setCurrentCardIndex(currentCardIndex + 1);
+  };
+
+  const handleBackCard = () => {
+    setShowAnswer(false);
+    setGuess("");
+    setFeedback(null);
+    setCurrentCardIndex(currentCardIndex - 1);
+  };
+
+  const isBackDisabled = currentCardIndex === 0;
+  const isNextDisabled = currentCardIndex === cards.length - 1;
+
+  const handleGuessSubmit = (e) => {
+    e.preventDefault();
+    if (guess.toLowerCase() === currentCard.answer.toLocaleLowerCase()) {
+      setFeedback("Correct!");
+    } else {
+      setFeedback("Incorrect.");
+    }
+  };
 
   return (
     <div className="App">
       <div className="header">
-          <h1>The Ultimate Plant Parent!</h1>
-          <h3>How good of a plant parent are you? Test all of your planty knowledge here!</h3>
-          <h3>Number of cards: {cards.length}</h3>
+        <h1>The Ultimate Plant Parent!</h1>
+        <h3>
+          How good of a plant parent are you? Test all of your planty knowledge
+          here!
+        </h3>
+        <h3>Number of cards: {cards.length}</h3>
 
-          <Card
-            question={currentCard.question}
-            answer={currentCard.answer}
-            image={currentCard.image}
-            category={currentCard.category}
-            difficulty={currentCard.difficulty}
-            showAnswer={showAnswer}
-            onCardClick={() => setShowAnswer(true)}/>
-          <button onClick={handleNextCard}>Next</button>
+        <Card
+          question={currentCard.question}
+          answer={currentCard.answer}
+          image={currentCard.image}
+          category={currentCard.category}
+          difficulty={currentCard.difficulty}
+          showAnswer={showAnswer}
+          onCardClick={() => setShowAnswer(true)}
+          guess={guess}
+          onGuessChange={(e) => setGuess(e.target.value)}
+          onGuessSubmit={handleGuessSubmit}
+          feedback={feedback}
+        />
+        <button onClick={handleBackCard} disabled={isBackDisabled}>
+          Back
+        </button>
+        <button onClick={handleNextCard} disabled={isNextDisabled}>
+          Next
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,37 +1,72 @@
-import React from 'react'
-import './Card.css'
+import React from "react";
+import "./Card.css";
 
 const Card = (props) => {
-	const {question, answer, showAnswer, onCardClick, image, category, difficulty} = props;
+  const {
+    question,
+    answer,
+    showAnswer,
+    onCardClick,
+    image,
+    category,
+    difficulty,
+    guess,
+    onGuessChange,
+    onGuessSubmit,
+    feedback,
+	// onBack,
+	// onNext,
+	// isBackDisabled,
+	// isNextDisabled
+  } = props;
 
-	const getDifficultyColor = () => {
-		switch (difficulty) {
-			case 'Easy':
-				return 'green';
-			case 'Medium':
-				return 'orange';
-			case 'Hard':
-				return 'red'
-			default:
-				return 'white'
-		}
-	}
+  const getDifficultyColor = () => {
+    switch (difficulty) {
+      case "Easy":
+        return "green";
+      case "Medium":
+        return "orange";
+      case "Hard":
+        return "red";
+      default:
+        return "white";
+    }
+  };
 
-	const cardStyle = {
-		backgroundColor: getDifficultyColor()
-	}
+  const cardStyle = {
+    backgroundColor: getDifficultyColor(),
+  };
 
- 	return (
-	  <div className='container'>
-	   <div className='card' onClick={onCardClick} style={cardStyle}>
-			<div className='card-content'>
-				
-				<div className='card-text'>{showAnswer ? answer : question}</div>
-				{image && <img src={image} alt="card image" className="card-image"/>}
-			</div>
-	   </div>
-	  </div>
-	)
-  }
+  return (
+	<>
+    <div className="container">
+      <div className="card" style={cardStyle} onClick={onCardClick}>
+        <div className="card-content">
+          <div className="card-text">{showAnswer ? answer : question}</div>
+          {image && <img src={image} alt="card image" className="card-image" />}
+        </div>
+		{/* <div className="navigation-buttons">
+          <button onClick={onBack} disabled={isBackDisabled}>
+            Back
+          </button>
+          <button onClick={onNext} disabled={isNextDisabled}>
+            Next
+          </button>
+        </div> */}
+      </div>
   
+    </div>
+	<div className="inputSubmit">
+        {!showAnswer && (
+          <form onSubmit={onGuessSubmit}>
+            <input type="text" value={guess} onChange={onGuessChange} />
+            <button type="submit">Submit</button>
+            {feedback && <div>{feedback}</div>}
+          </form>
+        )}
+      </div>
+	</>
+  );
+};
+
 export default Card;
